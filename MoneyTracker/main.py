d82c284d70c0a8a03258b40ec6191903c79c9f21
JacobRenxi <100037550+JacobRenxi@users.noreply.github.com>
2023-12-05 20:42:20 -0500

    Update main.py
    add login to my code

--- MoneyTracker/main.py
+++ MoneyTracker/main.py
@@ -1,41 +1,17 @@
 import sqlite3
 import matplotlib.pyplot as plt
 from datetime import datetime
+import login
 
 
-conn = sqlite3.connect('expenses.db')
-cursor = conn.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS expenses (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        category VARCHAR,
-        amount FLOAT,
-        description TEXT,
-        date TEXT
-    )
-''')
-conn.commit()
-
-def lowercase_categories(categories):
-    return [category.lower() for category in categories]
-
-'''
-Below is the categories that we can pick!
-Add more or change categories below!
-'''
-valid_categories = ['Food', 'Transportation', 'Housing', 'Entertainment', 'Utilities', 'Other']
-valid_categories_lower = lowercase_categories(valid_categories)
-
-
-def add_expense(category, amount, description, date):
+def add_expense(category, amount, description, date, cursor, conn):
     cursor.execute('''
         INSERT INTO expenses (category, amount, description, date)
         VALUES (?, ?, ?, ?)
     ''', (category, amount, description, date))
     conn.commit()
 
-def view_expenses():
+def view_expenses(cursor):
     cursor.execute('''
         SELECT * FROM expenses
     ''')
@@ -43,7 +19,7 @@ def view_expenses():
     for expense in expenses:
         print(expense)
 
-def generate_report():
+def generate_report(cursor):
     cursor.execute('''
         SELECT category, SUM(amount) as total_amount
         FROM expenses
@@ -61,9 +37,62 @@ def generate_report():
     plt.axis('equal')
     plt.show()
 
-def main():
+def login_here():
+    conn = sqlite3.connect('users.db')
+    login.create_user_table(conn)
+
+    print("Welcome to Expense Tracker!")
+    print("1. Login")
+    print("2. Register")
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        username = login.user_login(conn)
+        return username
+    elif choice == '2':
+        new_username = input("Enter new username: ")
+        new_password = input("Enter new password: ")
+        login.create_user(conn, new_username, new_password)
+        print("User registered successfully. Please login.")
+        username = login.user_login(conn)
+        return username
+    else:
+        print("Invalid choice. Exiting.")
+        conn.close()
+
+def main():    
+    username = login_here()
+    # After successful login, use the username for database connection
+    user_db_name= f'{username}_expenses.db'
+
+    conn = sqlite3.connect(user_db_name)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category VARCHAR,
+            amount FLOAT,
+            description TEXT,
+            date TEXT
+        )
+    ''')
+    conn.commit()
+
+    def lowercase_categories(categories):
+        return [category.lower() for category in categories]
+
+    '''
+    Below is the categories that we can pick!
+    Add more or change categories below!
+    '''
+    valid_categories = ['Food', 'Transportation', 'Housing', 'Entertainment', 'Utilities', 'Other', 'ADD*']
+    valid_categories_lower = lowercase_categories(valid_categories)
+
     max_budget = float(input("Enter your maximum budget: "))
     current_budget = max_budget
+
+
 
     while True:
         print("\nExpense Tracker")
@@ -85,26 +114,36 @@ def main():
                 print("Invalid category. Valid categories are:", valid_categories)
                 category = input("Enter a valid category: ")
 
-            amount = float(input("Enter amount: "))
-            if amount > current_budget:
-                print("Amount exceeds current budget. Please enter a smaller amount.")
-                continue
+            if category == 'add*':
+                amount = float(input("Enter amount adding to current budget: "))
+                current_budget += amount
+                print("Budget updated successfully!")
+                description = input("Enter description: ")
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                add_expense(category, amount, description, timestamp, cursor, conn)
+                print("Budget added successfully!")
 
-            description = input("Enter description: ")
+            else:
+                amount = float(input("Enter amount: "))
+                if amount > current_budget:
+                    print("Amount exceeds current budget. Please enter a smaller amount.")
+                    continue
 
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            add_expense(category, amount, description, timestamp)
-            current_budget -= amount
-            print("Expense added successfully!")
+                description = input("Enter description: ")
 
-            # Display valid categories after adding an expense
-            print("\nValid categories:", valid_categories)
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                add_expense(category, amount, description, timestamp, cursor, conn)
+                current_budget -= amount
+                print("Expense added successfully!")
+
+                # Display valid categories after adding an expense
+                print("\nValid categories:", valid_categories)
 
         elif choice == '2':
             print("\nAll Expenses:")
-            view_expenses()
+            view_expenses(cursor)
         elif choice == '3':
-            generate_report()
+            generate_report(cursor)
         elif choice == '4':
             break
         else:
